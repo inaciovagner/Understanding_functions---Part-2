bec990bf9d08843410d2cf6c5f9b5d617669845e
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-22 16:53:55 +0100

    Add project files.

---
+++ Understanding_functions - Part 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CsharpPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            Add(x, y);
+        }
+        static void Add(int x, int y)
+        {
+            int sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
